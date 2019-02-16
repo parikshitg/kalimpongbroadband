79cf07950dd6baa04e3b19fa4a8eba2df5b86888
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-02-17 00:54:53 +0530

    products page v1.0

--- views/funcs.go
+++ views/funcs.go
@@ -1,8 +1,8 @@
 package views
 
 import (
+	"html/template"
 	"log"
-	"text/template"
 )
 
 // FuncMap passes functions to templates
@@ -14,4 +14,9 @@ var FuncMap = template.FuncMap{
 func ActiveURI(uri1 string, uri2 string) bool {
 	log.Println("uri1:", uri1, " uri2:", uri2)
 	return uri1 == uri2
+}
+
+// HtmlString compares two urls
+func HtmlString(s string) interface{} {
+	return template.HTML(s)
 }
